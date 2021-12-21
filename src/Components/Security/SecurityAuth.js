a3cc512bc704fded4936381666b617c6e362c068
Ethan Bowen <ethan.bowen@macys.com>
2021-12-21 09:53:41 -0600

    Cognito user management update

--- src/Components/Security/SecurityAuth.js
+++ src/Components/Security/SecurityAuth.js
@@ -4,28 +4,37 @@ import { configureAmplify } from '../../Utilities/auth';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import Security from './Security.js';
 import awsconfig from '../../aws-exports';
+import { validateSession } from '../../Utilities/auth.js'
 
 
 // one time configuration for authentication
 if (!Amplify.Auth.userPool) {
     Amplify.configure(awsconfig);
-    //configureAmplify();
 }
 
 const SecurityAuth = () => {
-    const { user, setUser } = useState()
+    const [cognitoUser, setCognitoUser] = useState();
+
+    // Only using this approach to trigger a re-render
+    // for when Amplify initializes 
+    useEffect(() => {
+        Auth.currentAuthenticatedUser().then(
+            cognitoUser => {
+                if (!cognitoUser) {
+                    Auth.federatedSignIn({ provider: 'Google' }); //redirects to Google sign in
+                } else {
+                    setCognitoUser(cognitoUser)
+                }
+            }, error => {
+                console.log(error);
+                Auth.federatedSignIn({ provider: 'Google' }); //redirects to Google sign in
+            })
+    }, [])
 
     return (
         <>
-            !user ?
-            <div className="App">
-                <>{Auth.federatedSignIn({ provider: 'Google' })}</>
-
-                { }
-            </div> :
-            <Security />
+            {!cognitoUser ? <div>Redirecting...</div> : <Security />}
         </>
-
     )
 }
 
